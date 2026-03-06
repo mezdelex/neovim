f47d5a56d8e71968167cec7c6b752d7bf1f12622
mezdelex <alexcondegomez@gmail.com>
2026-03-07 00:52:37 +0100

    chore(mason): move mason dependency setup call to where it belongs for consistency

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,72 +1,71 @@
 return {
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"angularls",
-				"basedpyright",
-				"bashls",
-				"buf_ls",
-				"cssls",
-				"docker_compose_language_service",
-				"docker_language_server",
-				"dockerls",
-				"gopls",
-				"helm_ls",
-				"html",
-				"jsonls",
-				"lemminx",
-				"lua_ls",
-				"marksman",
-				"powershell_es",
-				"ruff",
-				"rust_analyzer",
-				"sqlls",
-				"terraformls",
-				"tombi",
-				"ts_ls",
-				"vimls",
-				"vue_ls",
-				"yamlls",
-			},
-		})
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "angularls",
+                "basedpyright",
+                "bashls",
+                "buf_ls",
+                "cssls",
+                "docker_compose_language_service",
+                "docker_language_server",
+                "dockerls",
+                "gopls",
+                "helm_ls",
+                "html",
+                "jsonls",
+                "lemminx",
+                "lua_ls",
+                "marksman",
+                "powershell_es",
+                "ruff",
+                "rust_analyzer",
+                "sqlls",
+                "terraformls",
+                "tombi",
+                "ts_ls",
+                "vimls",
+                "vue_ls",
+                "yamlls",
+            },
+        })
 
-		vim.diagnostic.config({
-			float = {
-				focus = false,
-				scope = "cursor",
-			},
-			jump = { on_jump = vim.diagnostic.open_float },
-			signs = {
-				numhl = {
-					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-				},
-				text = {
-					[vim.diagnostic.severity.ERROR] = "",
-					[vim.diagnostic.severity.HINT] = "",
-					[vim.diagnostic.severity.INFO] = "",
-					[vim.diagnostic.severity.WARN] = "",
-				},
-			},
-			update_in_insert = true,
-			virtual_text = true,
-		})
+        vim.diagnostic.config({
+            float = {
+                focus = false,
+                scope = "cursor",
+            },
+            jump = { on_jump = vim.diagnostic.open_float },
+            signs = {
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                    [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+                    [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+                    [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+                },
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.HINT] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                },
+            },
+            update_in_insert = true,
+            virtual_text = true,
+        })
 
-		vim.keymap.set("n", "<leader>R", "<cmd>Roslyn restart<cr>")
-	end,
-	defer = true,
-	dependencies = {
-		{
-			defer = true,
-			src = "https://github.com/mezdelex/roslyn.nvim",
-		},
-		{
-			defer = true,
-			src = "https://github.com/williamboman/mason-lspconfig.nvim",
-		},
-	},
-	src = "https://github.com/neovim/nvim-lspconfig",
+        vim.keymap.set("n", "<leader>R", "<cmd>Roslyn restart<cr>")
+    end,
+    defer = true,
+    dependencies = {
+        {
+            defer = true,
+            src = "https://github.com/mezdelex/roslyn.nvim",
+        },
+        {
+            defer = true,
+            src = "https://github.com/williamboman/mason-lspconfig.nvim",
+        },
+    },
+    src = "https://github.com/neovim/nvim-lspconfig",
 }
