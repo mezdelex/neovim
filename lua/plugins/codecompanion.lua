b8fdacdd7f8d39d2a83cfcfd15421ab8c51acfaa
mezdelex <alexcondegomez@gmail.com>
2026-03-11 18:08:10 +0100

    chore(plugins): update plugins; switch back to strict ollama adapter

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,22 +1,15 @@
 return {
-	config = function()
-		local codecompanion = require("codecompanion")
+    config = function()
+        local codecompanion = require("codecompanion")
 
-		codecompanion.setup({
-			interactions = {
-				chat = { adapter = os.getenv("CODECOMPANION_ADAPTER") },
-			},
-		})
-		vim.cmd("Copilot disable")
+        codecompanion.setup({
+            interactions = {
+                chat = { adapter = "ollama" },
+            },
+        })
 
-		vim.keymap.set("n", "<leader>h", codecompanion.toggle)
-	end,
-	defer = true,
-	dependencies = {
-		{
-			defer = true,
-			src = "https://github.com/github/copilot.vim",
-		},
-	},
-	src = "https://github.com/olimorris/codecompanion.nvim",
+        vim.keymap.set("n", "<leader>h", codecompanion.toggle)
+    end,
+    defer = true,
+    src = "https://github.com/olimorris/codecompanion.nvim",
 }
