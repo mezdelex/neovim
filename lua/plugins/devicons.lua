74db3abd628eecbac0ca19f071fdd9dcb2bbd608
mezdelex <alexcondegomez@gmail.com>
2026-03-13 18:30:32 +0100

    chore(devicons): remove custom config for devicons since defaults are enough and it gets loaded by plugin

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -1,10 +1,4 @@
 return {
-	config = function()
-		require("nvim-web-devicons").setup({
-			color_icons = true,
-			default = true,
-		})
-	end,
-	defer = true,
-	src = "https://github.com/nvim-tree/nvim-web-devicons",
+    defer = true,
+    src = "https://github.com/nvim-tree/nvim-web-devicons",
 }
