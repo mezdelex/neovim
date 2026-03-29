8856a51326e337f6635b841e64d5cbc76f8dc0de
mezdelex <alexcondegomez@gmail.com>
2026-03-29 21:36:51 +0200

    chore(agentic): override default AgenticTitle highlight to integrate it better with everforest theme

--- lua/plugins/agentic.lua
+++ lua/plugins/agentic.lua
@@ -6,6 +6,12 @@ return {
             provider = "opencode-acp",
         })
 
+        vim.api.nvim_set_hl(0, "AgenticTitle", {
+            bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg,
+            bold = true,
+            fg = vim.api.nvim_get_hl(0, { name = "String" }).fg,
+        })
+
         vim.keymap.set("n", "<leader>h", agentic.toggle)
     end,
     defer = true,
