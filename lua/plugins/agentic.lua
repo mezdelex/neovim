5f0fbfdd65e75c30c7c4a52f55c746721b38b48e
mezdelex <alexcondegomez@gmail.com>
2026-04-01 20:31:02 +0200

    chore(agentic): override icons with custom ones

--- lua/plugins/agentic.lua
+++ lua/plugins/agentic.lua
@@ -3,7 +3,29 @@ return {
         local agentic = require("agentic")
 
         agentic.setup({
+            chat_icons = {
+                agent = "",
+                user = "",
+            },
+            diagnostic_icons = {
+                error = "",
+                hint = "",
+                info = "",
+                warn = "",
+            },
+            permission_icons = {
+                allow_always = "󱍷",
+                allow_once = "",
+                reject_always = "",
+                reject_once = "󰍶",
+            },
             provider = "opencode-acp",
+            status_icons = {
+                completed = "",
+                failed = "",
+                in_progress = "",
+                pending = "",
+            },
         })
 
         vim.keymap.set("n", "<leader>h", agentic.toggle)
