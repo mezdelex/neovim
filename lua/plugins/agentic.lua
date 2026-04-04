6aa5b56f38c54e69d8d93f0702e9f55649f97c4e
mezdelex <alexcondegomez@gmail.com>
2026-04-04 20:32:02 +0200

    chore(agentic): add more modes to the toggle keymap to allow different functionalities

--- lua/plugins/agentic.lua
+++ lua/plugins/agentic.lua
@@ -38,7 +38,7 @@ return {
             },
         })
 
-        vim.keymap.set("n", "<leader>h", agentic.toggle)
+        vim.keymap.set({ "i", "n", "x" }, "<a-h>", agentic.toggle)
     end,
     defer = true,
     src = "https://github.com/carlos-algms/agentic.nvim",
