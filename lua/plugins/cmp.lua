7562333fae4baa2a62c771286cd399cb9429e585
mezdelex <alexcondegomez@gmail.com>
2026-05-14 18:52:06 +0200

    fix(cmp): fix cmp sources using only the necessary ones to avoid meaningless or unrelated suggestions; update plugins

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,25 +2,25 @@ return {
     config = function()
         local cmp = require("cmp")
         local sources = {
-            { name = "nvim_lsp" },
-            { name = "buffer" },
-            { name = "cmdline" },
-            { name = "path" },
+            buffer = { name = "buffer" },
+            cmdline = { name = "cmdline" },
+            nvim_lsp = { name = "nvim_lsp" },
+            path = { name = "path" },
         }
 
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
                 ["<cr>"] = cmp.mapping.confirm({ select = true }),
             }),
-            sources = cmp.config.sources(sources),
+            sources = cmp.config.sources({ sources.nvim_lsp, sources.buffer, sources.path }),
         })
         cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources(sources),
+            sources = cmp.config.sources({ sources.cmdline, sources.path }),
         })
         cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources(sources),
+            sources = cmp.config.sources({ sources.buffer }),
         })
     end,
     defer = true,
