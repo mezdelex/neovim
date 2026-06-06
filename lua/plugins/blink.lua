87b78bb30c7431542f8558fdeb175c9764022086
mezdelex <alexcondegomez@gmail.com>
2026-06-06 17:53:47 +0200

    chore(plugins): update plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,40 +1,40 @@
 return {
-    config = function()
-        local cmp = require("blink.cmp")
-        local pairs = require("blink.pairs")
+	config = function()
+		local cmp = require("blink.cmp")
+		local pairs = require("blink.pairs")
 
-        cmp.build():pwait()
-        pairs.build():pwait()
+		cmp.build():pwait()
+		pairs.build():pwait()
 
-        require("blink.indent").setup({
-            scope = {
-                highlights = { "BlinkIndentScope" },
-            },
-        })
-        cmp.setup({
-            completion = {
-                documentation = { auto_show = true },
-            },
-            keymap = { preset = "enter" },
-        })
-        pairs.setup({
-            highlights = { enabled = false },
-        })
-    end,
-    defer = true,
-    dependencies = {
-        {
-            defer = true,
-            src = "https://github.com/saghen/blink.lib",
-        },
-        {
-            defer = true,
-            src = "https://github.com/saghen/blink.indent",
-        },
-        {
-            defer = true,
-            src = "https://github.com/saghen/blink.pairs",
-        },
-    },
-    src = "https://github.com/saghen/blink.cmp",
+		require("blink.indent").setup({
+			scope = {
+				highlights = { "BlinkIndentScope" },
+			},
+		})
+		cmp.setup({
+			completion = {
+				documentation = { auto_show = true },
+			},
+			keymap = { preset = "enter" },
+		})
+		pairs.setup({
+			highlights = { enabled = false },
+		})
+	end,
+	defer = true,
+	dependencies = {
+		{
+			defer = true,
+			src = "https://github.com/saghen/blink.lib",
+		},
+		{
+			defer = true,
+			src = "https://github.com/saghen/blink.indent",
+		},
+		{
+			defer = true,
+			src = "https://github.com/saghen/blink.pairs",
+		},
+	},
+	src = "https://github.com/saghen/blink.cmp",
 }
